155fa82e79ea4300930d0e96c49977d292bff2e2
Thomas Smith <thom@thomthesmith.com>
2019-08-20 17:39:31 -0400

    Add media keys.

--- commands.lua
+++ commands.lua
@@ -1,6 +1,13 @@
 local fnutils = require('hs.fnutils')
 local Window = dofile('window.lua')
 
+local event = require('hs.eventtap.event')
+
+function systemKey(key)
+    event.newSystemKeyEvent(key, true):post()
+    event.newSystemKeyEvent(key, false):post()
+end
+
 return {
     escape = {hs.reload},
 
@@ -18,4 +25,16 @@ return {
         fnutils.partial(Window.move, { 0,   0,   1, 1 }),
         Window.fullScreen,
     },
+
+    pageup   = {
+        fnutils.partial(systemKey, 'SOUND_UP'),
+        fnutils.partial(systemKey, 'BRIGHTNESS_UP'),
+    },
+    pagedown = {
+        fnutils.partial(systemKey, 'SOUND_DOWN'),
+        fnutils.partial(systemKey, 'BRIGHTNESS_DOWN'),
+    },
+
+    f15      = {fnutils.partial(systemKey, 'MUTE')}, -- Pause
+    help     = {fnutils.partial(systemKey, 'PLAY')}, -- Insert
 }
