93093ebe7406e6474cb8689d7a2f0a9c3001d592
Thom Smith <thom@thomthesmith.com>
2022-02-03 10:57:43 -0500

    Use icon for menu.

--- menu.lua
+++ menu.lua
@@ -1,8 +1,28 @@
 local menubar = require('hs.menubar')
 local Commands = dofile('commands.lua')
 
+-- http://cocoamine.net/blog/2015/03/20/replacing-photoshop-with-nsstring/
+local ICON = [[
+· · · · · · · · · · · · · · · ·
+7 8 · · · · · · · · · 8 6 · · ·
+· · · · · · · · · · · · 6 · · ·
+· · 1 · · · · · · · · · · · 1 ·
+· · 3 · · · · · · · · · · · 4 ·
+· · · · · · · · b · · · · · · ·
+· · · · · · · a z c · · · · · ·
+· · · · · m · · · · · i · · · ·
+· · · · p y · · · · · y j · · ·
+· · · · · n · · · · · k · · · ·
+7 · · · · · · e z g · · · · · ·
+5 5 · · · · · · f · · · · · · ·
+· · 3 · · · · · · · · · · · 4 ·
+· · 2 · · · · · · · · · · · 2 ·
+· · · · · · · · · · · · · · · ·
+· · · · · · · · · · · · · · · ·
+]]
+
 menu = menubar.new(true)
-menu:setTitle('HS')
+menu:setIcon('ASCII:' .. ICON)
 menu:setMenu({
     {
         title = 'Resize Terminal',
